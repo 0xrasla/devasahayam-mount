409882eaee1c41f3dcd6062f2df5d95e3bf69be3
MichealGeorgeJ <michealmount23@gmail.com>
2025-01-08 00:41:21 +0530

    Gallery responsiveness achived

--- client/src/components/gallery.tsx
+++ client/src/components/gallery.tsx
@@ -1,34 +1,59 @@
 export default function Gallery() {
+  const images = [
+    { src: "1.jpg", alt: "Maniyadichan Parai" },
+    { src: "2.jpg", alt: "Maniyadichan Parai" },
+    { src: "5.jpg", alt: "Maniyadichan Parai" },
+    { src: "4.jpg", alt: "Maniyadichan Parai" },
+    { src: "5.jpg", alt: "Maniyadichan Parai" },
+    { src: "4.jpg", alt: "Maniyadichan Parai" },
+    { src: "1.jpg", alt: "Maniyadichan Parai" },
+    { src: "2.jpg", alt: "Maniyadichan Parai" },
+    { src: "5.jpg", alt: "Maniyadichan Parai" },
+    { src: "4.jpg", alt: "Maniyadichan Parai" },
+    { src: "5.jpg", alt: "Maniyadichan Parai" },
+    { src: "4.jpg", alt: "Maniyadichan Parai" },
+  ];
+
   return (
-    <section id="gallery" className="py-2 overflow-y-scroll">
-      <h2 className="text-[27px] text-black font-bold px-4 py-2">Gallery</h2>
-      <div className="container mx-auto px-4 flex gap-2 w-[100vh]">
-        <img
-          src={"1.jpg"}
-          alt={"Maniyadichan Parai"}
-          className="h-[300px] object-cover rounded-lg"
-        />
+    <section id="gallery" className="py-2 h-full  ">
 
-        <div className="flex flex-col gap-2">
-          <img
-            src={"2.jpg"}
-            alt={"Maniyadichan Parai"}
-            className="h-[150px] w-[300px] object-cover rounded-lg"
-          />
+<h2 className="text-[27px]  text-black font-bold md:w-[87%] md:mx-auto mx-6  py-2">Gallery</h2>
 
-          <img
-            src={"6.jpg"}
-            alt={"Maniyadichan Parai"}
-            className="h-[150px] w-[300px] object-cover rounded-lg"
-          />
-        </div>
 
-        <img
-          src={"4.jpg"}
-          alt={"Maniyadichan Parai"}
-          className="h-[300px] object-cover rounded-lg"
-        />
-      </div>
+
+<div className=" md:w-[87%] md:mx-auto mx-6 px-4 py-2 overflow-x-auto scrollbar-width-none">
+<div className="flex   gap-2">
+{images.map((image, index) => {
+
+const positionInChunk = (index % 3) + 1;
+console.log(positionInChunk);
+
+if(positionInChunk === 3 ) {
+  return null;
+}
+if(positionInChunk === 2 ) {
+  return (
+    <>
+    <div className="flex flex-col gap-2">
+      <img src={image?.src} alt="" className="min-w-16 max-w-48 md:min-w-48 md:max-w-64 lg:min-w-64 lg:max-w-72 object-cover rounded-lg aspect-auto h-full" />
+      <img src={images[index + 1]?.src} alt="" className="min-w-16 md:min-w-48 md:max-w-64 lg:min-w-64 lg:max-w-72 max-w-48 object-cover rounded-lg aspect-auto h-full" />
+    </div>
+    
+    </>
+    );
+}
+
+return (
+<>
+<div className="">
+  <img src={image?.src} alt="" className="min-w-24 max-w-64 h-full  object-cover rounded-lg md:min-w-64 md:max-w-72 lg:min-w-64 lg:max-w-96  aspect-auto" />
+</div>
+
+</>
+);
+})}
+</div>
+</div>
     </section>
   );
 }
