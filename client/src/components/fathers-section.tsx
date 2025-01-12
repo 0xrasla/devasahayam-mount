f9ee5c471d8ceb4a3922ba5e04c58531b6777cbe
MichealGeorgeJ <michealmount23@gmail.com>
2025-01-12 16:06:30 +0530

    fathers section design completed

--- client/src/components/fathers-section.tsx
+++ client/src/components/fathers-section.tsx
@@ -1,30 +1,130 @@
+import React from "react";
+import Slider from "react-slick";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import { useNavigate } from "@tanstack/react-router";
+
 export function FathersSection() {
+  const navigate = useNavigate();
+
   const fathersInfo = [
     {
       name: "Rev.Fr. S. Leon Henson",
+      desc: "A brief description about Rev.Fr. S. Leon Henson.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
     },
     {
       name: "Rev.Fr. J. Saleen Johnrose",
+      desc: "A brief description about Rev.Fr. J. Saleen Johnrose.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
     },
     {
       name: "Rev.Fr. Ravi",
+      desc: "A brief description about Rev.Fr. Ravi.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
     },
     {
       name: "Dn. Yesu Pravin",
+      desc: "A brief description about Dn. Yesu Pravin.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
+    },
+    {
+      name: "Rev.Fr. J. Saleen Johnrose",
+      desc: "A brief description about Rev.Fr. J. Saleen Johnrose.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
+    },
+    {
+      name: "Rev.Fr. Ravi",
+      desc: "A brief description about Rev.Fr. Ravi.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
+    },
+    {
+      name: "Dn. Yesu Pravin",
+      desc: "A brief description about Dn. Yesu Pravin.",
+      img: "https://i.ibb.co/DWr70FP/Whats-App-Image-2025-01-12-at-08-35-34.jpg",
     },
   ];
 
-  return (
-    <section id="visit" className="">
-      <div className="bg-white py-2">
-        <div className="max-w-6xl px-5 md:max-w-[90%] md:mx-auto">
-          <h1 className="text-[27px] md:text-[30px] font-bold py-4l">
-            Management
-          </h1>
-        </div>
 
-        <div></div>
-      </div>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <section id="visit" className="p-3 md:px-0  md:w-[90%] md:mx-auto mx-auto ">
+      <h2 className="text-[27px]  text-black font-bold  md:px-0 md:mx-auto mx-2 lg:mx-4 lg:px-0  py-2 ">Management</h2>
+      <Slider  {...settings}>
+        {fathersInfo.map((father, index) => (
+          <div key={index} className=" min-w-72  h-96 p-2 cursor-pointer">
+            <Card className="  h-full bg-white border-none custom-shadow">
+              <CardHeader>
+                <CardTitle>{father.name}</CardTitle>
+                {father.img ? (
+                  <img
+                    src={father.img}
+                    alt={father.name}
+                    className="w-full h-48 object-contain bg-white rounded-lg mt-2"
+                  />
+                ) : (
+                  <div className="w-full h-48 flex items-center justify-center bg-muted rounded-lg">
+                    No Image Available
+                  </div>
+                )}
+              </CardHeader>
+              <CardContent>
+                <CardDescription>
+                  {father.desc || "Description not available."}
+                </CardDescription>
+              </CardContent>
+
+            </Card>
+          </div>
+        ))}
+      </Slider>
+      <Button
+        onClick={() => {
+          navigate({
+            to: "/fathers",
+          });
+        }}
+        className="capitalize font-bold text-start h-[42px] w-[116px] bg-accent hover:bg-white text-[16px] rounded-lg  cursor-pointer hover:text-accent border hover:border-accent mt-7 md:mt-4 "
+      >
+        View more...
+      </Button>
     </section>
   );
 }
--- client/src/components/gallery.tsx
+++ client/src/components/gallery.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "@tanstack/react-router";
+import { Button } from "./ui/button";
+
 export default function Gallery() {
+  const navigate = useNavigate();
   const images = [
     { src: "1.jpg", alt: "Maniyadichan Parai" },
     { src: "2.jpg", alt: "Maniyadichan Parai" },
@@ -13,52 +17,58 @@ export default function Gallery() {
     <section id="gallery" className="py-2 h-full  ">
 
 
-<h2 className="text-[27px]  text-black font-bold md:w-[90%] md:px-0 md:mx-auto mx-4 lg:px-0  py-2">Gallery</h2>
+      <h2 className="text-[27px]  text-black font-bold md:w-[90%] md:px-0 md:mx-auto mx-4 lg:px-0  py-2">Gallery</h2>
 
 
 
-<div className=" md:w-[90%] md:mx-auto mx-4 md:px-0 lg:px-0 py-2 overflow-x-auto scrollbar-width-none">
-<div className="flex   gap-2">
-{images.map((image, index) => {
+      <div className=" md:w-[90%] md:mx-auto mx-4 md:px-0 lg:px-0 py-2 overflow-x-auto scrollbar-width-none">
+        <div className="flex   gap-2">
+          {images.map((image, index) => {
 
-const positionInChunk = (index % 3) + 1;
-console.log(positionInChunk);
+            const positionInChunk = (index % 3) + 1;
+
 
             if (positionInChunk === 3) {
               return null;
             }
             if (positionInChunk === 2) {
               return (
-                <>
-                  <div className="flex flex-col gap-2">
-                    <img
-                      src={image?.src}
-                      alt=""
-                      className="min-w-16 max-w-48 md:min-w-48 md:max-w-64 lg:min-w-64 lg:max-w-72 object-cover rounded-lg aspect-square h-full"
-                    />
-                    <img
-                      src={images[index + 1]?.src}
-                      alt=""
-                      className="min-w-16 md:min-w-48 md:max-w-64 lg:min-w-64 lg:max-w-72 max-w-48 object-cover rounded-lg aspect-square h-full"
-                    />
-                  </div>
-                </>
+                <div key={`chunk-${index}`} className="flex flex-col gap-2">
+                  <img
+                    src={image?.src}
+                    alt=""
+                    className="min-w-16 max-w-48 md:min-w-48 md:max-w-64 lg:min-w-64 lg:max-w-72 object-cover rounded-lg aspect-square h-full"
+                  />
+                  <img
+                    src={images[index + 1]?.src}
+                    alt=""
+                    className="min-w-16 md:min-w-48 md:max-w-64 lg:min-w-64 lg:max-w-72 max-w-48 object-cover rounded-lg aspect-square h-full"
+                  />
+                </div>
               );
             }
 
             return (
-              <>
-                <div className="">
-                  <img
-                    src={image?.src}
-                    alt=""
-                    className="min-w-24 max-w-64 h-full  object-cover rounded-lg md:min-w-64 md:max-w-72 lg:min-w-64 lg:max-w-96  aspect-square"
-                  />
-                </div>
-              </>
+              <div key={`single-${index}`} className="">
+                <img
+                  src={image?.src}
+                  alt=""
+                  className="min-w-24 max-w-64 h-full  object-cover rounded-lg md:min-w-64 md:max-w-72 lg:min-w-64 lg:max-w-96  aspect-square"
+                />
+              </div>
             );
           })}
         </div>
+        <Button
+          onClick={() => {
+            navigate({
+              to: "/gallery",
+            });
+          }}
+          className="capitalize font-bold text-start h-[42px] w-[116px] bg-accent hover:bg-white text-[16px] rounded-lg mt-3 cursor-pointer hover:text-accent border hover:border-accent "
+        >
+          View more...
+        </Button>
       </div>
     </section>
   );
